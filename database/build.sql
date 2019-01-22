14b297355959ba8559237d3cde6c1bbb732b470e
Micah Halter <micah@mehalter.com>
2019-01-22 16:49:27 -0500

    Jot forms page (#9)
    * filtering and sorting algorithm implemented, need more concrete implementation
    
    * Searching and filtering feature implented, updated icon
    
    * Added ability to ingest some CSV
    
    * IT WORKKS!!!
    
    * Query data now gets passed to the renderer successfully. Yaigit add *
    
    * Now uses asynchronous ipc calls. still errors tho :(
    
    * I think this is how the promises should work.
    
    * Added a bit of README, must have gotten deleted when we cleaned, added code to query database with joins for presenter/copresenters, and removed ctags from git tracking
    
    * SQL still doesn't work. Ive updated it with my latest attempts
    
    * Created an sequelize model for the presentations
    
    * Fixed IPC, now need to fix SQL
    
    * Boiler table builds, ipc not passing sql query reults back.
    
    * "send is not a function" error resovled.
    
    * NOT WORKING. Working on IPC signals.
    
    * Fixed the merge issues
    
    * Outlined IPC for populating the table with the query
    
    * changed pop presentations, console output of results.
    
    * Automatically Generates a table and inserts it into the DOM
    
    * initial table construction ideas for sql transaction and DOM
    
    * Working JotForms data-ingestion, going to clean up the code a lot before calling it done
    
    * updated ui to fit rubric
    
    * Updated Frontend UI
    
    * added import file button and secondary header jotforms page
    
    * Removed code that breaks the master branch
    
    * Working on parsing csv
    
    * Changed how config is untracked
    
    * Started working on csv ingestion, pushing a blank config file so I can make it untrackable
    
    * Updated build sql file
    
    * Added some more metadata and added some dependencies for the project that I know so far. More code to come utilizing said dependencies
    
    * Update index.html
    
    * Fix root
    
    * rename to electron application name
    
    * Electron Application start
    
    * created build script for ideas postgresql database

---
+++ database/build.sql
@@ -0,0 +1,80 @@
+CREATE EXTENSION citext;
+
+DROP SCHEMA IF EXISTS ideas CASCADE;
+CREATE SCHEMA ideas;
+
+CREATE TABLE IF NOT EXISTS ideas.attendee (
+  id serial primary key,
+  creation_date timestamp default current_timestamp,
+  prefix text,
+  first text NOT NULL,
+  last text NOT NULL,
+  email citext NOT NULL,
+  phone text,
+  agency text NOT NULL, -- might be another table with foreign key when we learn more
+  role text, -- might be another table with foreign key when we learn more
+  -- Add more attendee fields when we see eventmobi registration process
+  UNIQUE(prefix, first, last)
+);
+
+CREATE TABLE IF NOT EXISTS ideas.category (
+  id serial primary key,
+  title text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ideas.reviewer (
+  id serial primary key,
+  first text NOT NULL,
+  last text NOT NULL,
+  UNIQUE(first, last)
+);
+
+CREATE TABLE IF NOT EXISTS ideas.review (
+  id serial primary key,
+  reviewer_id integer REFERENCES ideas.reviewer(id) NOT NULL
+  -- need to put required fields
+);
+
+CREATE TABLE IF NOT EXISTS ideas.presentation (
+  id serial primary key,
+  submission_date timestamp NOT NULL,
+  title text NOT NULL UNIQUE,
+  description text NOT NULL,
+  objective_1 text NOT NULL,
+  objective_2 text NOT NULL,
+  objective_3 text NOT NULL,
+  repurposed_agreement boolean NOT NULL,
+  setup_agreement boolean NOT NULL,
+  equipment_agreement boolean NOT NULL,
+  backup_agreement boolean NOT NULL,
+  timelimit_agreement boolean NOT NULL,
+  time_agreement boolean NOT NULL,
+  handout_agreement boolean NOT NULL,
+  acceptance_agreement boolean NOT NULL,
+  registration_agreement boolean NOT NULL,
+  copyright_agreement boolean NOT NULL,
+  --posting_agreement boolean NOT NULL,
+  vendor boolean NOT NULL,
+  vendor_agreement boolean CONSTRAINT need_vendor CHECK(vendor = (vendor_agreement IS NOT NULL)),
+  comments text,
+  presenter_id integer REFERENCES ideas.attendee(id) NOT NULL,
+  presenter_biography text NOT NULL,
+  copresenter_1_id integer REFERENCES ideas.attendee(id),
+  copresenter_2_id integer REFERENCES ideas.attendee(id),
+  copresenter_3_id integer REFERENCES ideas.attendee(id),
+  time tstzrange,
+  room text,
+  category_id integer REFERENCES ideas.category(id),
+  accepted boolean,
+  CONSTRAINT unique_presenters CHECK (
+    (copresenter_1_id IS NULL OR (copresenter_1_id != presenter_id)) AND
+    (copresenter_2_id IS NULL OR (copresenter_1_id IS NOT NULL AND copresenter_2_id != presenter_id AND copresenter_2_id != copresenter_1_id)) AND
+    (copresenter_3_id IS NULL OR (copresenter_1_id IS NOT NULL AND copresenter_2_id IS NOT NULL AND copresenter_3_id != presenter_id AND copresenter_3_id != copresenter_1_id AND copresenter_3_id != copresenter_2_id))
+  )
+);
+
+CREATE TABLE IF NOT EXISTS ideas.presentation_review (
+  presentation_id integer REFERENCES ideas.presentation(id) NOT NULL,
+  review_id integer REFERENCES ideas.review(id) NOT NULL UNIQUE,
+  primary key (presentation_id, review_id)
+);
---
+++ ideas-conference-planner/index.js
@@ -0,0 +1,184 @@
+const ipc = require('electron').ipcRenderer;
+
+function getAttendeeName(attendee) {
+  return attendee == null ? "" : (attendee.prefix == null ? "" : attendee.prefix) + ' ' + attendee.first + ' ' + attendee.last;
+}
+
+function generateTable(data) {
+  var presentationDiv = document.getElementById('presentationDiv');
+  var table = document.createElement('table');
+  table.setAttribute('border','1');
+  table.setAttribute('width','100%');
+  table.setAttribute('id', 'table');
+  var numRows = data.length;
+  var row = table.insertRow(0);
+
+  var th = document.createElement('th');
+  th.appendChild(document.createTextNode('Date'));
+  row.appendChild(th);
+  th.onclick= function(){sortTable(0);} ; 
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Title'));
+  th.onclick= function(){sortTable(1);} ; 
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Description'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Objective 1'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Objective 2'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Objective 3'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Presenter'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Co-Presenter 1'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Co-Presenter 2'));
+  row.appendChild(th);
+
+  th = document.createElement('th');
+  th.appendChild(document.createTextNode('Co-Presenter 3'));
+  row.appendChild(th);
+
+  for (i = 0; i < numRows; ++i) {
+    var row = table.insertRow(i + 1);
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(data[i].submission_date.slice(0, 10)));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(data[i].title));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(data[i].description));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(data[i].objective_1));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(data[i].objective_2));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(data[i].objective_3));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(getAttendeeName(data[i].Presenter)));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(getAttendeeName(data[i].Copresenter1)));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(getAttendeeName(data[i].Copresenter2)));
+    row.appendChild(td);
+
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(getAttendeeName(data[i].Copresenter3)));
+    row.appendChild(td);
+
+  }
+  presentationDiv.innerHTML = '';
+  presentationDiv.appendChild(table);
+}
+
+function refreshPresentations() {
+  ipc.send('query-presentations', '');
+}
+
+//document.addEventListener('DOMContentLoaded', generateTable); //should be refreshPresentations
+document.addEventListener('DOMContentLoaded', refreshPresentations);
+
+document.getElementById("getjotfile").addEventListener("change", ingestCSV);
+
+function ingestCSV() {
+  var jotfile = document.getElementById("getjotfile");
+  ipc.send('ingest-csv', jotfile.files[0].path);
+}
+
+ipc.on('ingest-csv', function(event, arg) {
+  alert(arg);
+});
+
+ipc.on('query-presentations-reply', function(event, arg) {
+  var query = JSON.parse(arg);
+  generateTable(query);
+});
+
+function sortTable(n) {
+        console.log("sorting")
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        table = document.getElementById("table");
+        switching = true;
+        dir = "asc"; 
+        while (switching) {
+            switching = false;
+            rows = table.getElementsByTagName("tr");
+            for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch= true;
+                break;
+            }
+        } else if (dir == "desc") {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch= true;
+            break;
+            }
+        }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount ++; 
+        } else {
+        if (switchcount == 0 && dir == "asc") {
+            dir = "desc";
+            switching = true;
+        }
+        }
+        }
+    }
+function searchFunc() {
+  // Declare variables 
+  console.log("searching");
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
