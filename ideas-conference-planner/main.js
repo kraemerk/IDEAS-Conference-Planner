76c11ff127cad0b6ba3e8aa5e5114013aa049ef7
Micah Halter <micah.halter@gtri.gatech.edu>
2019-01-24 18:15:21 -0500

    Started working on csv ingestion, pushing a blank config file so I can make it untrackable

--- ideas-conference-planner/main.js
+++ ideas-conference-planner/main.js
@@ -1,8 +1,33 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
 function createWindow () {
-  win = new BrowserWindow({ width: 800, height: 600 })
-  win.loadFile('index.html')
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile('index.html');
 }
 
-app.on('ready', createWindow)
+function ingestCSV (file) {
+  var csv = require('csv-streamify');
+  var fs = require('fs');
+
+  var options = {
+    delimiter : ',',
+    endLine : '\n',
+    quote : '`',
+    empty : '',
+    columns: true
+  }
+
+  var csvStream = csv(options);
+
+  fs.createReadStream(file).pipe(csvStream)
+    .on('error', function(err) {
+      console.error(err);
+    })
+    .on('data',function(data) {
+      console.log(data);
+    })
+}
+
+ingestCSV('/home/micah/Documents/School-Work/CS3312/presentations.csv');
+
+app.on('ready', createWindow);
