d99bba39a5e4eb181190ebf86d70b653479cd283
drfarid <habichuelas21@gmail.com>
2019-02-16 17:55:03 -0500

    minor

--- ideas-conference-planner/categorizer.js
+++ ideas-conference-planner/categorizer.js
@@ -1,2 +1,13 @@
-functions to allow for the manipulation of the current
-presentation category to be changed
+// functions to allow for the manipulation of the current
+// presentation category to be changed
+
+function displayEntry(row) {
+	ipc.send('display-entry');
+	var title = row[1];
+	var header = document.getElementById('categorizedTitle');
+	header.append(title);
+}
+
+function updateCategorgy(rowID) {
+	ipc.send('update-category', rowID);
+}
