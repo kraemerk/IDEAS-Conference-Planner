db3225bdd4402235bb15e4a084c534e9fc8b7702
hwolleat <hlwolleat@gmail.com>
2019-02-17 13:47:48 -0500

    raw html readout on new page

--- ideas-conference-planner/index-rating.js
+++ ideas-conference-planner/index-rating.js
@@ -1,1 +1,6 @@
-window.alert('hello\n' + sessionStorage.row);
+
+var ratingDiv = document.getElementById('ratingDiv');
+
+var td = document.createElement('td');
+td.appendChild(document.createTextNode(sessionStorage.row));
+ratingDiv.appendChild(td);
