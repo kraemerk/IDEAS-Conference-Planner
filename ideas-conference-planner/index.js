53ba83f381c320ec35539b7cbae524ba90881f0e
Micah Halter <micah@mehalter.com>
2019-02-18 23:06:37 -0500

    Categorization (#11)
    * added onclick highlighting to table rows, added columns for ratings.
    
    * Creates child window when presentation is clicked.
    
    * kind of navigation between pages...
    
    * Working on navigation
    
    * Using Thomas's superior action system.
    
    * fixed multiple buttons and no hilights.
    
    * keeping branches separate.
    
    * added categorization button
    
    * minor
    
    * minor
    
    * ipc
    
    * minor
    
    * Cleaned some things up, and got rid of that horrible navigation hack...
    
    * minor changes
    
    * minor
    
    * Reverted some changes that got overritten
    
    * Passes data between pages
    
    * Also added a back button, if we ever need that functionality...
    
    * minor
    
    * used session storage
    
    * display the entry without parsing
    
    * selected entry displays
    
    * made a modal window
    
    -made modal window
    -removed new page functionality
    
    * made the modal display nicer
    
    * made a for loop for the modal window
    
    used to be a bunch of repeated statements. Wasn't working earlier. is now.
    
    * cleaned up
    
    removed old commented out code
    
    * fixed button bug in modal window
    
    * added addcategorization method
    
    * added dropdown
    
    * the category space clears with every click
    
    * added sequelize
    
    * removed categorize page and js file
    
    * starting to add sql functionality
    
    this push doesn't work. take out lines 4 through seven and it'll run like before.
    
    * removed code from wrong place
    
    * commented out non-working code
    
    * fixed disappearance of dropdown menu
    
    * added edit categories pic
    
    * resized edit icon
    
    * fixed appearance and disappearance of buttons and dropdowns in category space
    
    * idea to use session storage to pass sequelize object
    
    * working on appearing and disappearing items
    
    * categorization without sql working
    
    * added markers where sql needs to happen
    
    * fixed dropdown
    
    it now shows the previously selected category at the top
    
    * Removed file that shouldn't be there
    
    * created category entry with value and id
    
    * added code to load the table with dummy entries
    
    * added functionality to send categories through ipc and to the index file
    
    * commented out alert debugger in index js 341
    
    * working on sequelize
    
    * fixed extra button error
    
    * get id from category
    
    * Update index.js
    
    * recovered old code
    
    * recoveredold
    
    trying to get working code
    
    * old content
    
    having trouble reverting
    
    * now it works
    
    * Pass categorization data to main.js
    
    * restart the selected category
    
    * successfully sets category in database
    
    * Tried to get categories to display in table. Currently outputs undefined for everything
    
    * categorization WORKZZZZgit add index.js

--- ideas-conference-planner/index.js
+++ ideas-conference-planner/index.js
@@ -1,8 +1,211 @@
 const ipc = require('electron').ipcRenderer;
+
+var clickedCategory = false;
+
+var clickedEdit = false;
+
+var changedValue = false;
+
+var categoryList;
+
+var selectedCategory;
+
 var tb = null;
+
 function getAttendeeName(attendee) {
+
   return attendee == null ? "" : (attendee.prefix == null ? "" : attendee.prefix) + ' ' + attendee.first + ' ' + attendee.last;
+
 }
+
+function getCategoryFromId(categoryID) {
+  if (categoryID == null)
+    return "";
+  
+  for (var i = 0; i < categoryList.length; ++i) {
+    if (categoryList[i].id == categoryID)
+      return categoryList[i].title;
+  }
+  return "";
+}
+
+
+
+function ratePresentation(rowID) {
+
+  var button = document.createElement('button');
+
+  button.textContent = 'Rate';
+
+  var actionSpace = document.getElementById('actions' + rowID);
+
+
+
+  button.addEventListener('click', () => {
+
+    window.location = 'index-rating.html'
+
+  }, false);
+
+
+
+  actionSpace.appendChild(button);
+
+}
+
+
+
+function addCategorization(rowID) {
+
+  //gets the place where the items are to be added
+
+  var categorySpace = document.getElementById('categorySpace' + rowID);
+
+  clickedCategory = false;
+
+  clickedEdit = false;
+
+  changedValue = false;
+
+
+
+  // selectedCategory = categorySpace.innerHTML;
+
+  
+
+
+
+  //creates the dropdown menu and id's it by the row
+
+  var dropDownMenu = document.createElement("SELECT");
+
+  dropDownMenu.id = "categoryDropDown" + rowID;
+
+  dropDownMenu.options[dropDownMenu.selectedIndex] = categorySpace.innerHTML;
+
+  
+
+  //-------------------------------------------------------------------------------------> MICAH: add all entries in category table
+
+  //loop for every category and add an option
+
+  //for each one
+
+
+
+  for (i = 0; i < categoryList.length; i++) {
+
+    var option = document.createElement('option');
+
+    option.text = categoryList[i].title;
+
+    dropDownMenu.add(option);
+
+  }
+
+
+
+  
+
+  //create edit button with edit picture
+
+  var editButton = document.createElement('button');
+
+  editButton.innerHTML = "<img src='images/editCategories.png'/>";
+
+  
+
+  //when dropdown is clicked set the category value to the selected item
+
+  dropDownMenu.onclick = function() {
+
+    clickedCategory = true;
+
+  }
+
+
+
+  //when the value of the dropdown is changed
+
+  //the whole window should not reload
+
+  dropDownMenu.onchange = function() {
+
+    changedValue = true;
+
+    selectedCategory = dropDownMenu.options[dropDownMenu.selectedIndex].text;
+
+    dropDownMenu.value = selectedCategory;
+
+  
+
+
+
+    //----------------------------------------------------------------------------------------> MICAH: change the value of the presentation's category
+
+    //do sql query to change the value of the selected presentation's category
+
+    ipc.send('set-category',
+      {"presentation":document.getElementById(rowID).cells[2].innerHTML,
+      "category":categoryList[dropDownMenu.selectedIndex].id});
+  }
+
+
+
+  dropDownMenu.onblur = function() {
+
+    changedValue = false;
+
+    clickedEdit = false;
+
+    clickedCategory = false;
+
+  }
+
+
+
+  editButton.onblur = function() {
+
+    changedValue = false;
+
+    clickedEdit = false;
+
+    clickedCategory = false;
+
+  }
+
+
+
+
+
+
+
+  editButton.onclick = function() {
+
+    clickedEdit = true;
+
+    editCategories();
+
+  }
+
+
+
+  //add dropdown to the category space
+
+  categorySpace.appendChild(dropDownMenu);
+
+
+
+  //add the edit button to the category space
+
+  categorySpace.appendChild(editButton);
+
+
+
+
+
+}
+
 
 function getReviewer(review) {
   if (review[0] != null) {
@@ -44,60 +247,136 @@ function ratePresentation(rowID, presID) {
 }
 
 function generateTable(data) {
-  var presentationDiv = document.getElementById('presentationDiv');
-  var table = document.createElement('table');
-  table.id = 'PresentationTable';
 
+  var presentationDiv = document.getElementById('presentationDiv');
+
+  var table = document.createElement('table');
+
+  table.id = 'PresentationTable';
+  
   table.setAttribute('border','1');
+
   table.setAttribute('width','100%');
+
   table.setAttribute('id', 'table');
+
   var numRows = data.length;
+
   var row = table.insertRow(0);
 
+
+
+
+
+
+
   var th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Actions'));
   row.appendChild(th);
 
   th = document.createElement('th');
   th.appendChild(document.createTextNode('Date'));
+
   row.appendChild(th);
   th.onclick= function(){sortTable(0);} ; 
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Title'));
+
   th.onclick= function(){sortTable(1);} ; 
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Description'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Objective 1'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Objective 2'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Objective 3'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Presenter'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Co-Presenter 1'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Co-Presenter 2'));
+
   row.appendChild(th);
 
+
+
   th = document.createElement('th');
+
   th.appendChild(document.createTextNode('Co-Presenter 3'));
+
+  row.appendChild(th);
+
+
+
+  th = document.createElement('th');
+
+  th.appendChild(document.createTextNode('Rating 1'));
+
+  row.appendChild(th);
+
+
+
+  th = document.createElement('th');
+
+  th.appendChild(document.createTextNode('Rating 2'));
+
+  row.appendChild(th);
+
+
+
+  th = document.createElement('th');
+
+  th.appendChild(document.createTextNode('Category'));
+
   row.appendChild(th);
 
   th = document.createElement('th');
@@ -109,7 +388,9 @@ function generateTable(data) {
   row.appendChild(th);
 
   for (i = 0; i < numRows; ++i) {
+
     var row = table.insertRow(i + 1);
+
     row.id =  i;
 
     var td = document.createElement('td');
@@ -119,43 +400,111 @@ function generateTable(data) {
 
 
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(data[i].submission_date.slice(0, 10)));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(data[i].title));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(data[i].description));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(data[i].objective_1));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(data[i].objective_2));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(data[i].objective_3));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(getAttendeeName(data[i].Presenter)));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(getAttendeeName(data[i].Copresenter1)));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(getAttendeeName(data[i].Copresenter2)));
+
     row.appendChild(td);
 
+
+
     td = document.createElement('td');
+
     td.appendChild(document.createTextNode(getAttendeeName(data[i].Copresenter3)));
+
+    row.appendChild(td);
+
+
+
+    td = document.createElement('td');
+
+    td.appendChild(document.createTextNode(getAttendeeName(data[i].Rating1)));
+
+    row.appendChild(td);
+
+
+
+    td = document.createElement('td');
+
+    td.appendChild(document.createTextNode(getAttendeeName(data[i].Rating2)));
+
+    row.appendChild(td);
+
+
+
+
+
+    //-------------------------------------------------------------------------------------------->MICAH: show the category of the current presentation
+
+    td = document.createElement('td');
+
+    td.id = 'categorySpace' + i;
+
+    td.appendChild(document.createTextNode(getCategoryFromId(data[i].category_id)));
+
     row.appendChild(td);
 
     td = document.createElement('td');
@@ -196,29 +545,59 @@ function generateTable(data) {
   }
 
   presentationDiv.innerHTML = '';
+
   presentationDiv.appendChild(table);
+
+}
+
+
+function pageLoad() {
+  ipc.send('get-categories', '');
 }
 
 
 function refreshPresentations() {
+
   ipc.send('query-presentations', '');
+
 }
 
 document.addEventListener('DOMContentLoaded', refreshPresentations);
 
+
 document.getElementById("getjotfile").addEventListener("change", ingestCSV);
 
+
+
 function ingestCSV() {
+
   var jotfile = document.getElementById("getjotfile");
+
   ipc.send('ingest-csv', jotfile.files[0].path);
+
 }
 
+
+
 ipc.on('ingest-csv', function(event, arg) {
+
   alert(arg);
+
 });
 
+
+
+ipc.on('get-categories-reply', function(event, arg) {
+  categoryList = JSON.parse(arg);
+  refreshPresentations();
+});
+
+
+
 ipc.on('query-presentations-reply', function(event, arg) {
+
   var query = JSON.parse(arg);
+
   generateTable(query);
 });
 
