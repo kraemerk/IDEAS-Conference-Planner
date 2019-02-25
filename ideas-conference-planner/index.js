83a5c2a8790087a9d3f3e4da53cd7a4064388c42
drfarid <habichuelas21@gmail.com>
2019-02-24 20:04:39 -0500

    added the modal window again

--- ideas-conference-planner/index.js
+++ ideas-conference-planner/index.js
@@ -442,7 +442,38 @@ function generateTable(data) {
       }
     }
 
+    row.ondblclick = function() {
 
+      var pEntry = document.getElementById('pEntry');
+
+      pEntry.innerHTML = '';
+      var length = this.cells.length - 1;
+      var nextCell = document.createElement('p');
+      
+      for (i = 0; i < length; i++) {
+        if (i != 13 && i != 0) {
+          nextCell.innerHTML = this.cells[i].innerHTML;
+          pEntry.appendChild(nextCell);
+          nextCell = document.createElement('p');
+        }
+      }
+
+      var modal = document.getElementById('myModal');
+      var span = document.getElementsByClassName("close")[0];
+
+      modal.style.display = "block";
+
+
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    }
   }
 
   presentationDiv.innerHTML = '';
