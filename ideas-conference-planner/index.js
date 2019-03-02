3e7eb463dec5158cfc495bd29e4bccef7fa48ba2
drfarid <habichuelas21@gmail.com>
2019-03-02 12:10:07 -0500

    debugging the category counter

--- ideas-conference-planner/index.js
+++ ideas-conference-planner/index.js
@@ -1,4 +1,4 @@
-  const ipc = require('electron').ipcRenderer;
+const ipc = require('electron').ipcRenderer;
 
 var categoryList;
 
@@ -111,7 +111,6 @@ function populateCategoryCountList() {
   categoryCountList = [];
 
 
-
   for (i = 0; i < categoryList.length; i++ ) {
     // alert("i: " + i + " Title: " + categoryList[i].title + " ID:" + getCategoryIdFromName(categoryList[i].title));
     ipc.send('get-category-count', getCategoryIdFromName(categoryList[i].title));
@@ -129,10 +128,14 @@ function addCategorization(rowID) {
   var dropDownMenu = document.createElement("SELECT");
   dropDownMenu.id = "categoryDropDown" + rowID;
 
+  var option = document.createElement('option');
+  option.text = 'Select Category';
+  dropDownMenu.add(option);
+
   //loops for every category in category list and
   //ads a dropdown option for each one
   for (i = 0; i < categoryList.length; i++) {
-    var option = document.createElement('option');
+    option = document.createElement('option');
     option.text = categoryList[i].title;
     dropDownMenu.add(option);
   }
@@ -143,10 +146,11 @@ function addCategorization(rowID) {
     selectedCategory = dropDownMenu.options[dropDownMenu.selectedIndex].text;
     dropDownMenu.value = selectedCategory;
     currentCategorySpace.innerHTML = selectedCategory;
+    var sIndex = dropDownMenu.selectedIndex - 1;
 
     ipc.send('set-category',
       {"presentation":document.getElementById(rowID).cells[2].innerHTML,
-      "category":categoryList[dropDownMenu.selectedIndex].id});
+      "category":categoryList[sIndex].id});
 
     //when the category is changed, the categorycount list must be reinitialized
     location.reload();
@@ -181,8 +185,10 @@ function addCategorizationActions(rowID) {
   deleteButton.id = 'deleteCategory' + rowID;
 
   //gets the number of presentations in order to check if delete button should be added
-  var presentationCount = document.getElementById('presentationCount' + rowID);
-  presentationCount.innerHTML = categoryCountList[rowID];
+  var presentationCount = document.getElementById('presentationCount' + rowID).innerHTML;
+  // presentationCount.innerHTML = categoryCountList[rowID];
+
+  var pCount = presentationCount.innerHTML;
 
   //when the edit button is clicked the user should be allowed to modify the text
   //in the selected category and they will be shown a button to save and a button to cancel
@@ -197,7 +203,7 @@ function addCategorizationActions(rowID) {
   //if there are zero presentations
   //delete the category with a query
   //and redraw the table without the old category
-  if (presentationCount == 0) {
+  if (pCount == 0) {
     deleteButton.onclick = function() {
       var cTtitle = document.getElementById('categoryValue' + rowID).innerHTML;
       var cID = getCategoryIdFromName(cTtitle);
@@ -245,10 +251,10 @@ function editCategory() {
 
 
   for (i = 0; i < length; i++) {
-    if (i != 0) {
+    // if (i != 0) {
 
       //create a new row and give it id i
-      var newRow = categoryTable.insertRow(i);
+      var newRow = categoryTable.insertRow(i+1);
       newRow.id = i;
 
       //this cell will hold the category value at categorylist[i]
@@ -262,7 +268,7 @@ function editCategory() {
       var td = document.createElement('td');
 
       td.id = 'presentationCount' + i;
-      td.appendChild(document.createTextNode(categoryCountList[i]));
+      td.appendChild(document.createTextNode(categoryList[i].title + " count: " + categoryCountList[i]));
       newRow.appendChild(td);
 
 
@@ -298,7 +304,7 @@ function editCategory() {
           } 
           tb = this;            
         }
-      }
+      // }
     }
   }
 
@@ -317,8 +323,6 @@ function editCategory() {
       modal.style.display = "none";
     }
   }
-
-  // populateCategoryCountList();
 }
 
 
